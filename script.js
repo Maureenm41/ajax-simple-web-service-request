9b2c404d995de7f7d0f23c29c2809e9204305d37
Maureenm41 <maureenmrchl@gmail.com>
2021-02-26 10:57:14 +0100

    work in console.log

---
+++ script.js
@@ -0,0 +1,13 @@
+let xhr = new XMLHttpRequest;
+
+xhr.open('GET','https://thatsthespir.it/api',true)
+
+xhr.onload = function()
+{
+    if(this.status === 200)
+    {
+        console.log(JSON.parse(this.responseText))
+    }
+}
+
+xhr.send();
