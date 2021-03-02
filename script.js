7c368b2fd5572c0df9d87b9ee8d4cbcf9fe37fb7
Maureenm41 <maureenmrchl@gmail.com>
2021-03-02 17:11:04 +0100

    test with splice (don't work)

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ xhr.onload = function()
     if(this.status === 200)
     {
         console.log(JSON.parse(this.responseText)); 
-        document.getElementById('quoteText').innerHTML= this.responseText.slice(9,150);
+        document.getElementById('quoteText').innerHTML= this.responseText.splice(quote,author);
     }
 }
 
