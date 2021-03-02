6e085b7ced2d50b7df14f841cd63afd65fdf5b36
Maureenm41 <maureenmrchl@gmail.com>
2021-03-02 17:11:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ xhr.onload = function()
     if(this.status === 200)
     {
         console.log(JSON.parse(this.responseText)); 
-        document.getElementById('quoteText').innerHTML= this.responseText.splice(quote,author);
+        document.getElementById('quoteText').innerHTML= this.responseText.splice(1,5);
     }
 }
 
