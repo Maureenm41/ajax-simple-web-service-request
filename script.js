ab85eb58073d4d61fba4b36b56c3d630320a7c96
Maureenm41 <maureenmrchl@gmail.com>
2021-03-03 16:24:52 +0100

    btn new quote work

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 var xhr = new XMLHttpRequest;
 
-xhr.open('GET','https://thatsthespir.it/api',true)
+xhr.open('GET','https:thatsthespir.it/api',true)
 
-xhr.onload = function()
+xhr.onload = function() //donwload the quote
 {
     if(this.status === 200)
     {
         console.log(JSON.parse(this.responseText)); 
-        document.getElementById('quoteText').innerHTML= this.responseText.splice(1,5);
+        document.getElementById('quoteText').innerHTML= this.responseText.slice(9,150);
     }
 }
 
@@ -18,3 +18,13 @@ xhr.send();
 //document.getElementById('quote')= "hello";
 
 //document.getElementById('quote').innerHTML= this.responseText;
+
+
+//test 2
+
+fetch("https://thatsthespir.it/api")
+
+.then(res => res.json())
+.then( data => console.log(data))
+
+
