4fe4ea6a0abc0b055b6f21454a2935221a39355f
Maureenm41 <maureenmrchl@gmail.com>
2021-03-04 10:50:06 +0100

    test

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var xhr = new XMLHttpRequest;
 
-xhr.open('GET','https:thatsthespir.it/api',true)
+xhr.open('GET','https://thatsthespir.it/api',true)
 
 xhr.onload = function testing() //donwload the quote
 {
@@ -9,8 +9,6 @@ xhr.onload = function testing() //donwload the quote
         console.log(JSON.parse(this.responseText)); 
 
         document.getElementById('quoteText').innerHTML= this.responseText.slice(9,150);
-
-        
 
         fetch("https://thatsthespir.it/api")
 
@@ -33,28 +31,8 @@ xhr.onload = function testing() //donwload the quote
 }
 
 xhr.send();
-
-let quoteTest = xhr ;
 //document.getElementById('quote')= "hello";
 
-//test 2
-
-fetch("https://thatsthespir.it/api")
-
-.then(res => res.json())
-.then( data => console.log(data.quote))
-
-
-fetch("https://thatsthespir.it/api")
-
-.then(res => res.json())
-.then(data => console.log(data.author))
-
-
-fetch("https://thatsthespir.it/api")
-
-.then(res => res.json())
-.then(data => console.log(data.photo))
 
 
 
