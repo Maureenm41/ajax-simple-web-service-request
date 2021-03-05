5b40e13f439eb6609dcbffb989bf2909aaefff40
Maureenm41 <maureenmrchl@gmail.com>
2021-03-05 11:37:44 +0100

    test

--- script.js
+++ script.js
@@ -7,12 +7,13 @@ xhttp.onload = function ()  //donwload the quote
     if(this.status === 200)
     {
 
-        document.getElementById('quoteText').innerHTML= this.responseText.slice(9,500);
+        //document.getElementById('quoteText').innerHTML= this.responseText.slice(9,500);
 
         fetch("https://thatsthespir.it/api")
 
         .then(res => res.json())
         .then( data => console.log(data.quote))
+        .then(data => document.getElementById('quotetext').innerHTML=json.data[0].quote)
 
 
         fetch("https://thatsthespir.it/api")
@@ -25,14 +26,34 @@ xhttp.onload = function ()  //donwload the quote
         .then(res => res.json())
         .then(data => console.log(data.photo))
 
+        let jsonObj = req.responseText;
+        let profileJson = JSON.parse(jsonObj);
+        console.log(profileJson.data)
+    
+        document.getElementById("quoteText").innerHTML=profileJson.data[1].quote;
+        document.getElementById("author").innerHTML=profileJson.data[1].author;
+
         
     }
 }
 
 xhttp.send();
 
+//test2
 
-
-
-
-
+function getApi() 
+{
+    let req = new XMLHttpRequest();
+    req.open("GET","https://thatsthespir.it/api");
+    
+    req.onload=function()
+    {
+        let jsonObj = req.responseText;
+        let profileJson = JSON.parse(jsonObj);
+        console.log(profileJson.data)
+    
+        document.getElementById("quoteText").innerHTML=profileJson.data[1].quote;
+        document.getElementById("author").innerHTML=profileJson.data[1].author;
+    }
+    req.send();
+}
